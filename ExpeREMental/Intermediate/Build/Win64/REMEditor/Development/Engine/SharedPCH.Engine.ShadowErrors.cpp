bbbc7180f46f9492a27b4886d4dd21e8fb655c66
Jcortes97 <50885510+Jcortes97@users.noreply.github.com>
2021-03-12 16:29:26 -0800

    Bug fixes + movement mechanic touchups
    bug fixes
    qol stuff

---
+++ ExpeREMental/Intermediate/Build/Win64/REMEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.28.29336
+#include "C:/Users/Corte/OneDrive/Desktop/REM-Bug/Intermediate/Build/Win64/REMEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ ExpeREMental/Source/REM/REMGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "REMGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REM_API AREMGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
